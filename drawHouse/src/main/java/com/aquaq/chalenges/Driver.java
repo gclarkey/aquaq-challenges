21a96f8bc28fcc9da0a0a1d59561711527a2fc09
gclarkey <gclarkey@gmail.com>
2017-09-15 11:18:47 +0100

    Functionally complete. First pass before optimisation.

---
+++ drawHouse/src/main/java/com/aquaq/chalenges/Driver.java
@@ -0,0 +1,11 @@
+package com.aquaq.chalenges;
+
+public class Driver {
+
+    public static void main(String args[]){
+        for(int i =1; i<=50; i++){
+            final House house = new House(i);
+            house.drawHouse();
+        }
+    }
+}
---
+++ drawHouse/src/main/java/com/aquaq/chalenges/House.java
@@ -0,0 +1,52 @@
+package com.aquaq.chalenges;
+
+public class House {
+
+    final int height;
+
+    public House(final int height){
+        this.height = height;
+    }
+
+    public void drawHouse(){
+        drawRoof();
+        drawStructure();
+    }
+
+    private void drawRoof() {
+        for (int i=1; i<=height; i++) {
+            for(int j=0; j<height*2; j++){
+                if(j==height-i){
+                    System.out.print("/");
+                } else if (j==height+i-1){
+                    System.out.print("\\");
+                }
+                else {
+                    if (i==height) {
+                        System.out.print("_");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private void drawStructure() {
+        for (int i=height; i>0; i--) {
+            for(int j=0; j<height*2; j++){
+                if(j==0 || j==(height*2)-1){
+                    System.out.print("|");
+                } else {
+                    if (i==1) {
+                        System.out.print("_");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
