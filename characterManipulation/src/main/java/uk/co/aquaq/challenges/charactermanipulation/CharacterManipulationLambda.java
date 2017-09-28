00f69a079fe5c81d8b0a7723bb12dd447886841b
gclarkey <gclarkey@gmail.com>
2017-09-28 13:17:47 +0100

    switch char manip lambda over to use a stringbuilder.

--- characterManipulation/src/main/java/uk/co/aquaq/challenges/charactermanipulation/CharacterManipulationLambda.java
+++ characterManipulation/src/main/java/uk/co/aquaq/challenges/charactermanipulation/CharacterManipulationLambda.java
@@ -7,18 +7,18 @@ public class CharacterManipulationLambda {
     }
 
     public N n = a -> {                                   // Lambda with char-array parameter and String return-type
-        String r = "";                                    //  Result-String
+        final StringBuilder r = new StringBuilder();      //  Result-String
         int i = 0;                                        //  Flag for alteration
         for (int c : a)                                   //  Loop over the characters of the input
-            r += (char)                                   //   And append the result-String with the following (converted to char):
+            r.append( (char)                              //   And append the result-String with the following (converted to char):
                     (c > 64 & c < 91 | c > 96 & c < 123 ? //    If it's a letter:
                             i++ % 2 < 1 ?                 //     And the flag states it should be lowercase:
                                     (c | 32)              //      Convert it to lowercase
                                     :                     //     Else (should be uppercase):
                                     (c & ~32)             //      Convert it to uppercase
                             :                             //    Else:
-                            c);                           //     Simply append the non-letter character as is
-        //  End of loop (implicit / single-line body)
-        return r;                                         //  Return result-String
+                            c));                          //     Simply append the non-letter character as is
+                                                          //  End of loop (implicit / single-line body)
+        return r.toString();                              //  Return result-String
     };
 }
