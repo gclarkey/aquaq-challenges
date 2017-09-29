296a137b15d7536e81d56eaedd867c6a50682f39
gclarkey <gclarkey@gmail.com>
2017-09-29 14:25:36 +0100

    minified lambda

--- characterManipulation/src/main/java/uk/co/aquaq/challenges/charactermanipulation/CharacterManipulationLambda.java
+++ characterManipulation/src/main/java/uk/co/aquaq/challenges/charactermanipulation/CharacterManipulationLambda.java
@@ -2,7 +2,7 @@ package uk.co.aquaq.challenges.charactermanipulation;
 
 public class CharacterManipulationLambda {
 
-    public interface N{
+    public interface N {
         String c(char[] a);
     }
 
@@ -10,7 +10,7 @@ public class CharacterManipulationLambda {
         final StringBuilder r = new StringBuilder();      //  Result-String
         int i = 0;                                        //  Flag for alteration
         for (int c : a)                                   //  Loop over the characters of the input
-            r.append( (char)                              //   And append the result-String with the following (converted to char):
+            r.append((char)                              //   And append the result-String with the following (converted to char):
                     (c > 64 & c < 91 | c > 96 & c < 123 ? //    If it's a letter:
                             i++ % 2 < 1 ?                 //     And the flag states it should be lowercase:
                                     (c | 32)              //      Convert it to lowercase
@@ -18,7 +18,11 @@ public class CharacterManipulationLambda {
                                     (c & ~32)             //      Convert it to uppercase
                             :                             //    Else:
                             c));                          //     Simply append the non-letter character as is
-                                                          //  End of loop (implicit / single-line body)
+        //  End of loop (implicit / single-line body)
         return r.toString();                              //  Return result-String
     };
+
+String p(char[] a){String r="";int i=0;for(int c:a)r+=(char)c>64&c<91|c>96&c<123?i++%2<1?(c|32):(c&~32):c;return r;}
+
+
 }
